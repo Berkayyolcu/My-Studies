74d004a84e80aafaba5ac8a2d5b97ef7b1e09169
Berkay Yolcu <berkayyolcu99@gmail.com>
2024-04-21 19:08:41 +0300

    csharp proje eklendi

---
+++ Csharp/girilen 3 basamaklı sayıyı basamaklarına ayırma/girilen 3 basamaklı sayının basamaklarına ayıran/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace girilen_3_basamaklı_sayının_basamaklarına_ayıran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int sayi, yuzler, onlar, birler;
+            Console.Write(" Lütfen Bir Sayı Girin : ");           
+            sayi = Convert.ToInt32(Console.ReadLine());
+            yuzler = sayi / 100;
+            sayi = sayi - (yuzler * 100);
+            onlar = sayi / 10;
+            sayi = sayi - (onlar * 10);
+            birler = sayi;
+            Console.WriteLine("Yüzler basamağı : {0}", yuzler);
+            Console.WriteLine("Onlar basamağı : {0}", onlar);
+            Console.WriteLine("Birler basamağı : {0}", birler);
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Csharp/klavyeden girilen metnin kaç kelimeden oluştuğunu bulma/klavyeden girilen metnin kaç kelimeden oluştuğunu bulma/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Klavyeden_girilen_metnin_kaç_kelimeden_oluştuğunu_bulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string cumle;
+            Console.Write("Cümleyi Giriniz : ");
+            cumle = Console.ReadLine();
+            string[] kelimeler = cumle.Split(' ');
+            Console.WriteLine(kelimeler.Length);
+            Console.ReadLine();
+        }
+    }
+}
